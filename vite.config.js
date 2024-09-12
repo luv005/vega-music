f8db91a8aee1a55b64499fe492b9efbee2f2ce23
luv005 <luv005@proton.me>
2024-09-12 14:12:38 +0000

    fixed host

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3001,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -11,5 +12,8 @@ export default defineConfig({
         secure: false,
       }
     }
+  },
+  watchOptions: {
+    ignored: ['**/.pythonlibs/**', '**/node_modules/**']
   }
 });
